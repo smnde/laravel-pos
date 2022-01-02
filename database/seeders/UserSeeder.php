b8fbc07e5b644f14ac0a111db1f4668443c74805
err <des.mn69@gmail.com>
2022-01-02 15:32:29 +0700

    --

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,13 +16,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // $role = Role::create(['name' => 'admin']);
-        $role = Role::where('name', 'admin')->get();
+        $role = Role::create(['name' => 'admin']);
         $user = User::create([
             'name' => 'err',
             'username' => 'err',
             'password' => Hash::make('123qweasd'),
         ]);
-        $user->assignRole($role->name);
+        $user->assignRole($role);
     }
 }
