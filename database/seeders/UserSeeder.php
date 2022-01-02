c5259bd652ff743463543fe599f6579c1acf5203
err <des.mn69@gmail.com>
2022-01-02 17:23:54 +0700

    update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
@@ -16,7 +17,33 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
+        Permission::create(['name' => 'lihat user']);
+        Permission::create(['name' => 'tambah user']);
+        Permission::create(['name' => 'edit user']);
+        Permission::create(['name' => 'edit user']);
+        Permission::create(['name' => 'hapus user']);
+        Permission::create(['name' => 'lihat barang']);
+        Permission::create(['name' => 'tambah barang']);
+        Permission::create(['name' => 'edit barang']);
+        Permission::create(['name' => 'hapus barang']);
+        Permission::create(['name' => 'penjualan']);
+        Permission::create(['name' => 'pembelian']);
+        Permission::create(['name' => 'pengembalian']);
+        Permission::create(['name' => 'pembelian']);
+        Permission::create(['name' => 'cek histori']);
+
         $role = Role::create(['name' => 'admin']);
+        $role->givPermissionTo([
+            'lihat user',
+            'tambah user',
+            'edit user',
+            'hapus user',
+            'lihat barang',
+            'tambah barang',
+            'edit barang',
+            'hapus barang',
+        ]);
         $user = User::create([
             'name' => 'err',
             'username' => 'err',
