f4cd7ee2c83dc3f46bea40bc4bab16f45ef669dd
err <des.mn69@gmail.com>
2022-01-02 14:43:45 +0700

    adding UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create(['name' => 'admin']);
+        $user = User::create([
+            'name' => 'err',
+            'username' => 'err',
+            'password' => Hash::make('123qweasd'),
+        ]);
+        $user->assignRole($role);
+    }
+}
